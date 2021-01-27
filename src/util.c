94b49cb5edac1debe61ffaee1fa14e8c737fab8a
bratekarate <bratekannkarate@gmail.com>
2021-01-27 08:29:33 +0100

    improve tests

--- src/util.c
+++ src/util.c
@@ -6,6 +6,12 @@ void print_bytes(const char *bytes, const size_t len) {
   }
 }
 
+void print_bytes_int(const char *bytes, const size_t len) {
+  for (int i = 0; i < len; i++) {
+    printf("%d ", bytes[i]);
+  }
+}
+
 void print_hex_bytes(const char *bytes, const size_t len) {
   for (int i = 0; i < len; i++) {
     printf("%02X", bytes[i]);
@@ -20,17 +26,18 @@ void print_uint8(const u_int8_t *bytes, const size_t len) {
 
 void print_session(ParsedSession *session) {
   printf("salt: ");
-  print_bytes(session->salt, sizeof(session->salt));
+  print_bytes_int(session->salt, sizeof(session->salt));
   printf("\n");
   printf("vector: ");
-  print_bytes(session->vector, sizeof(session->vector));
+  print_bytes_int(session->vector, sizeof(session->vector));
   printf("\n");
   printf("rounds: %u\n", session->rounds);
-  printf("rest: %zu bytes", session->rest_size);
-  /*print_bytes(session->rest, session->rest_size);*/
+  // printf("rest: %zu bytes", session->rest_size);
+  printf("rest: ");
+  print_bytes_int(session->rest, session->rest_size);
   printf("\n");
   printf("hmac_sha256: ");
-  print_bytes(session->hmac_sha256, sizeof(session->hmac_sha256));
+  print_bytes_int(session->hmac_sha256, sizeof(session->hmac_sha256));
   printf("\n");
 }
 
--- test/tests.c
+++ test/tests.c
@@ -4,22 +4,54 @@ void test_parse();
 void test_print_to_file();
 void test_calc_aes_key();
 
-int main () {
-    test_parse();
+int main() {
+  test_parse();
+  // test_calc_aes_key();
 }
 
-void test_parse(){
-    ParsedSession *session = session_parse_alloc(stdin);
-    print_session(session);
+void test_parse() {
+  ParsedSession *session = session_parse_alloc(stdin);
+  print_session(session);
 
-    free(session->rest);
-    free(session);
+  char salt[SALT_LEN] = {
+      -23, 90, 59, 0, -58, 33, -22, 43, 79, -38, 41, -38, 31, 33, -73, -90, 0,
+  };
+  char vector[VECTOR_LEN] = {
+      -27, 43, -9, 83, 2, -114, 103, 83, -48, -105, 95, -17, -50, 56, 61,
+  };
+
+  char hmac_sha[HMAC_SHA256_LEN] = {
+      -7,  -5,  -43, -81, -11, 54,  -32, -98, 127, 14,   -24,
+      115, 102, -8,  7,   18,  78,  -5,  -18, 44,  -108, -29,
+      69,  -81, 49,  -98, -98, -44, 107, -70, 24,  -50,
+  };
+
+  uint32_t rounds = 500000;
+
+  // TODO: asserts (also rest)
+
+  free(session->rest);
+  free(session);
 }
 
 void test_print_to_file() {
-    // TODO
+  // TODO
 }
 
 void test_calc_aes_key() {
-    // TODO
+  char *salt = "ßZ;Ô!ê+OÝ)Ú!·¨";
+  char *passphrase = "testus";
+  uint32_t rounds = 500000;
+
+  const unsigned char *aes_key = calc_aes_key(passphrase, rounds, salt);
+
+  const unsigned char chrs[65] = {
+      36,  183, 144, 195, 202, 190, 234, 254, 33,  126, 14,  78,  207,
+      233, 146, 6,   171, 227, 83,  24,  26,  36,  27,  241, 73,  10,
+      187, 27,  42,  68,  108, 152, 23,  230, 132, 163, 144, 201, 36,
+      84,  74,  124, 117, 143, 120, 179, 217, 230, 87,  239, 227, 230,
+      72,  168, 57,  64,  185, 132, 168, 83,  75,  80,  238, 97,  0,
+  };
+
+  assert(!strcmp((char *)aes_key, (char *)chrs));
 }
