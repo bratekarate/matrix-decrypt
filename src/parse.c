05d747f8e4aa0079860e992ce5570c8b971a2355
bratekarate <bratekannkarate@gmail.com>
2021-01-29 22:50:59 +0100

    cleanup

--- src/parse.c
+++ src/parse.c
@@ -3,83 +3,78 @@
 ParsedSession *session_parse_alloc(FILE *fp) {
   ParsedSession *session = malloc(sizeof(ParsedSession));
 
-  char fstline[1000];
-  fgets(fstline, 1000, fp);
+  {
+    char buf[100];
+    fgets(buf, 100, fp);
+  }
 
-  char *content = malloc(1 * sizeof(char));
-  size_t content_size = 0;
+  size_t buf_size = 500;
+  char *content = malloc(buf_size * sizeof(char));
 
-  char chr;
-  while ((chr = fgetc(fp)) != EOF) {
-    if (chr != '\n') {
-      content = realloc(content, (content_size + 1) * sizeof(char));
-      content[content_size++] = chr;
+  size_t content_i = 0;
+  char buf;
+  while ((buf = fgetc(fp)) != EOF) {
+    if (buf != '\n') {
+      if ((content_i + 1) * sizeof(char) > buf_size) {
+        buf_size *= 1.5;
+        content = realloc(content, buf_size * sizeof(char));
+      }
+      content[content_i++] = buf;
     }
   }
 
-  char *stripped = malloc((content_size - 33) * sizeof(char));
-  memcpy(stripped, content, (content_size - 33) * sizeof(char));
+  const size_t payload_size = (content_i - 33) * sizeof(char);
+  char *stripped = malloc(payload_size);
+  memcpy(stripped, content, payload_size);
 
-  unsigned char *decoded = malloc((content_size - 33) * sizeof(char));
-  int size = b64_pton((const char *)stripped, decoded, (content_size - 33) * sizeof(char));
-
-  fclose(fp);
-
-  fp = fopen("/tmp/decoded", "w");
-  for (size_t i = 0; i < size; i++) {
-    fputc(decoded[i], fp);
-  }
+  unsigned char *decoded = malloc(payload_size);
+  int size = b64_pton((const char *)stripped, decoded, payload_size);
 
   fclose(fp);
 
-  fp = fopen("/tmp/decoded", "r");
+  unsigned char *dec_pointer = decoded;
+  session->format = *dec_pointer++;
 
-  session->format = fgetc(fp);
-
-  size_t i;
-  for (i = 0; i < SALT_LEN - 1; i++) {
-    session->salt[i] = fgetc(fp);
+  for (size_t i = 0; i < SALT_LEN - 1; i++) {
+    session->salt[i] = *dec_pointer++;
   }
-  session->salt[i] = '\0'; // \0 character necessary for python object conversion
+  session->salt[SALT_LEN - 1] =
+      '\0'; // \0 character necessary for python object conversion
 
-  for (i = 0; i < VECTOR_LEN; i++) {
-    session->vector[i] = fgetc(fp);
+  for (size_t i = 0; i < VECTOR_LEN; i++) {
+    session->vector[i] = *dec_pointer++;
   }
 
   u_int8_t rnd_arr[ROUNDS_LEN];
-  for (i = 0; i < ROUNDS_LEN; i++) {
-    rnd_arr[i] = fgetc(fp);
+  for (size_t i = 0; i < ROUNDS_LEN; i++) {
+    rnd_arr[i] = *dec_pointer++;
   }
 
   session->rounds =
-      (rnd_arr[0] << 24) + (rnd_arr[1] << 16) + (rnd_arr[2] << 8) +
-      rnd_arr[3];
+      (rnd_arr[0] << 24) + (rnd_arr[1] << 16) + (rnd_arr[2] << 8) + rnd_arr[3];
+  session->rest = malloc(sizeof(char));
 
-  i = 0;
-  session->rest = malloc(0);
-  char buf;
-  while (!feof(fp)) {
-    session->rest = realloc(session->rest, i + 1);
-    session->rest[i] = fgetc(fp);
-    i++; // TODO: why is i after loop the size of rest bytes + 1?
+  size_t rest_i;
+  for (rest_i = 0; rest_i < size + 1 - SALT_LEN - VECTOR_LEN - ROUNDS_LEN;
+       rest_i++) {
+    session->rest = realloc(session->rest, (rest_i + 1) * sizeof(char));
+    session->rest[rest_i] = *dec_pointer++;
   }
+  // TODO: why is i after loop the size of rest bytes + 1?
 
-  fclose(fp);
-  remove("/tmp/decoded");
-
-  if (i < HMAC_SHA256_LEN + 1) {
+  if (rest_i < HMAC_SHA256_LEN + 1) {
     fprintf(stderr, "Error: File content is too short.");
     exit(1);
   }
 
-  i--; // see TODO above
+  rest_i--; // see TODO above
 
-  size_t j;
-  for (j = 0; j < HMAC_SHA256_LEN; j++) {
-    session->hmac_sha256[HMAC_SHA256_LEN - 1 - j] = session->rest[i - 1 - j];
+  for (size_t i = 0; i < HMAC_SHA256_LEN; i++) {
+    session->hmac_sha256[HMAC_SHA256_LEN - 1 - i] =
+        session->rest[rest_i - 1 - i];
   }
 
-  session->rest_size = i - j;
+  session->rest_size = rest_i - HMAC_SHA256_LEN;
 
   return session;
 }
